d3718e661314e980f8292af14b9ce98c1cad4f20
Arie Roos <arie@itecho.co.za>
2019-08-27 20:02:25 +0200

    Improve state handling of doors
    Close #2

--- src/monty_hall/mod.rs
+++ src/monty_hall/mod.rs
@@ -22,50 +22,53 @@ pub fn play_game() {
     doors.print();
 }
 
+// Door states
+const WINNER: i8 = 1;
+const OPEN: i8 = 2;
+const CHOSEN: i8 = 4;
+
 /// Doors have the following states:
-///   0: closed (normal)
-///   1: closed (prize)
-///   2: open (garbage)
-///   3: open (prize)
-///   4: chosen (normal)
-///   5: chosen (prize)
-///   6: chosen (lost)
-///   7: chosen (won)
+///     0: closed (normal)
+///     1: closed (prize)
+///     2: open (garbage)
+///     3: open (prize)
+///     4: chosen (normal)
+///     5: chosen (prize)
+///     6: chosen (lost)
+///     7: chosen (won)
 struct Doors {
     doors: Vec<i8>,
-    has_winner: bool,
 }
 
 impl Doors {
     fn new() -> Doors {
         Doors {
             doors: vec![0, 0, 0],
-            has_winner: false,
         }
     }
 
+    fn clean(&mut self, disallowed: i8) {
+        let allowed = WINNER + OPEN + CHOSEN - disallowed;
+        self.doors = self.doors.iter().map(|d| d & allowed).collect();
+    }
+
     fn set_winner(&mut self) -> usize {
-        if self.has_winner {
-            panic!("This set of doors already has a winner!");
-        }
+        self.clean(WINNER);
 
         let winner: usize = thread_rng().gen_range(0, 3);
-        self.doors[winner] |= 1;
-        self.has_winner = true;
+        self.doors[winner] |= WINNER;
         winner + 1
     }
 
     fn choose_door(&mut self, choice: usize) {
-        // make sure no other door is chosen
-        self.doors = self.doors.iter().map(|d| d & 3).collect();
+        self.clean(CHOSEN);
 
         let i = choice - 1;
-        self.doors[i] |= 4;
+        self.doors[i] |= CHOSEN;
     }
 
-    fn print(&self)
-    {
-        for door in &self.doors{
+    fn print(&self) {
+        for door in &self.doors {
             match door {
                 0 | 1 => print!("[\u{25A0}]"),
                 2 => print!("[g]"),
@@ -74,7 +77,7 @@ impl Doors {
                 5 => print!("[*]"),
                 6 => print!("[G]"),
                 7 => print!("[P]"),
-                _ => panic!("Unknown door state!")
+                _ => panic!("Unknown door state!"),
             }
         }
         println!();
