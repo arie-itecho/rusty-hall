e783d450dc08298513ca74a4f801956f19b29f48
Arie Roos <arie@itecho.co.za>
2019-09-21 12:07:04 +0200

    Add menu

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
-
 mod custom_io;
 mod monty_hall;
+
 fn main() {
     println!("Welcome to Monty Hall! It looks a little rusty here...");
 
     loop {
-        monty_hall::play_game();
-        if !custom_io::read_yes_no("Play Again? ", Some(true)) {
-            break;
+        let menu = format!(
+            "{}\n{}\n{}\n{}\n{}",
+            "What do you want to do?",
+            " - (p)lay",
+            " - Simulate (m)any games",
+            " - Print (s)tats",
+            " - (q)uit"
+        );
+
+        let response = custom_io::read_str(&menu, &["p", "m", "s", "q"]);
+        match &response[..] {
+            "p" => monty_hall::play_game(),
+            "m" => continue, // TODO: Simulate many games
+            "s" => continue, // TODO: Print Stats
+            "q" => break,
+            _ => continue,
         }
     }
 }
